3f9d6f0a91942758667878551bf01d26bd13ca7b
curiositlyxd <curiositlyxd@gmail.com>
2022-06-23 01:23:49 -0800

    Add more helper files

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,7 +1,12 @@
 import asyncio
-from typing import Awaitable, Callable, ParamSpec, TypeVar
+import datetime
+import time
+from typing import Awaitable, Callable, Optional, ParamSpec, Sequence, TypeVar
 
-__all__ = ["cleanup_code", "to_thread", "plural"]
+from dateutil.relativedelta import relativedelta
+import discord
+
+__all__ = ["cleanup_code", "to_thread", "plural", "Timer"]
 
 T = TypeVar("T")
 P = ParamSpec("P")
@@ -34,3 +39,134 @@ def to_thread(func: Callable[P, T]) -> Callable[P, Awaitable[T]]:
         return await asyncio.to_thread(func, *args, **kwargs)
 
     return wrapper
+
+
+class Timer:
+    def __init__(self):
+        self._start = None
+        self._end = None
+
+    def start(self):
+        self._start = time.perf_counter()
+
+    def stop(self):
+        self._end = time.perf_counter()
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop()
+
+    def __int__(self):
+        return round(self.time)
+
+    def __float__(self):
+        return self.time
+
+    def __str__(self):
+        return str(self.time)
+
+    def __repr__(self):
+        return f"<Timer time={self.time}>"
+
+    @property
+    def time(self):
+        if self._end is None or self._start is None:
+            return 0
+
+        return self._end - self._start
+
+
+def human_join(seq: Sequence[str], delim: str = ", ", final: str = "or") -> str:
+    size = len(seq)
+    if size == 0:
+        return ""
+
+    if size == 1:
+        return seq[0]
+
+    if size == 2:
+        return f"{seq[0]} {final} {seq[1]}"
+
+    return delim.join(seq[:-1]) + f" {final} {seq[-1]}"
+
+
+def human_timedelta(
+    dt: datetime.datetime,
+    *,
+    source: Optional[datetime.datetime] = None,
+    accuracy: Optional[int] = 3,
+    brief: bool = False,
+    suffix: bool = True,
+) -> str:
+    now = source or datetime.datetime.now(datetime.timezone.utc)
+    if dt.tzinfo is None:
+        dt = dt.replace(tzinfo=datetime.timezone.utc)
+
+    if now.tzinfo is None:
+        now = now.replace(tzinfo=datetime.timezone.utc)
+
+    # Microsecond free zone
+    now = now.replace(microsecond=0)
+    dt = dt.replace(microsecond=0)
+
+    # This implementation uses relativedelta instead of the much more obvious
+    # divmod approach with seconds because the seconds approach is not entirely
+    # accurate once you go over 1 week in terms of accuracy since you have to
+    # hardcode a month as 30 or 31 days.
+    # A query like "11 months" can be interpreted as "!1 months and 6 days"
+    if dt > now:
+        delta = relativedelta(dt, now)
+        output_suffix = ""
+    else:
+        delta = relativedelta(now, dt)
+        output_suffix = " ago" if suffix else ""
+
+    attrs = [
+        ("year", "y"),
+        ("month", "mo"),
+        ("day", "d"),
+        ("hour", "h"),
+        ("minute", "m"),
+        ("second", "s"),
+    ]
+
+    output = []
+    for attr, brief_attr in attrs:
+        elem = getattr(delta, attr + "s")
+        if not elem:
+            continue
+
+        if attr == "day":
+            weeks = delta.weeks
+            if weeks:
+                elem -= weeks * 7
+                if not brief:
+                    output.append(format(plural(weeks), "week"))
+                else:
+                    output.append(f"{weeks}w")
+
+        if elem <= 0:
+            continue
+
+        if brief:
+            output.append(f"{elem}{brief_attr}")
+        else:
+            output.append(format(plural(elem), attr))
+
+    if accuracy is not None:
+        output = output[:accuracy]
+
+    if len(output) == 0:
+        return "now"
+    else:
+        if not brief:
+            return human_join(output, final="and") + output_suffix
+        else:
+            return " ".join(output) + output_suffix
+
+
+def format_relative(dt: datetime.datetime) -> str:
+    return discord.utils.format_dt(dt, "R")
