fcf88aede6f86486b66cbf25a9f48d4ca74652a8
curiositlyxd <curiositlyxd@gmail.com>
2022-07-19 10:33:45 -0800

    remove debug send

--- cogs/tools.py
+++ cogs/tools.py
@@ -579,7 +579,6 @@ class Tools(commands.Cog, name="tools"):
             )
 
         message = await ctx.send("Downloading video")
-        await ctx.send(basic_method)
 
         self.currently_downloading.append(f"{default_name}.{default_format}")
         start = time.perf_counter()
