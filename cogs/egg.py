4658782b9d411d15615e3284eefc64c73d3e3252
curiositly <curiositlyxd@gmail.com>
2022-08-02 12:18:50 -0700

    Fix command name

--- cogs/egg.py
+++ cogs/egg.py
@@ -12,9 +12,9 @@ class Egg(commands.Cog, name="egg"):
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    @commands.command(hidden=True)
+    @commands.command(hidden=True, aliases=('ei',))
     @commands.is_owner()
-    async def invite(self, ctx: GuildContext, users: int = 1):
+    async def egg_invite(self, ctx: GuildContext, users: int = 1):
         if ctx.guild.id != 1002190975965331477:
             return
 
