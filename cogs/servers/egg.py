8d120a222272f0548a9ab351aabe161e2d5cad93
curiositly <curiositlyxd@gmail.com>
2022-08-16 02:36:50 -0800

    Allow sending to certain user egg invite

--- cogs/servers/egg.py
+++ cogs/servers/egg.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import discord
 from bot import Bot
 from discord.ext import commands
@@ -14,16 +15,25 @@ class Egg(commands.Cog, name="egg"):
 
     @commands.command(hidden=True, aliases=("ei",))
     @commands.is_owner()
-    async def egg_invite(self, ctx: GuildContext, users: int = 1):
-        if ctx.guild.id != 1002190975965331477:
-            return
+    async def egg_invite(
+        self,
+        ctx: GuildContext,
+        uses: Optional[int] = 1,
+        member: discord.Member = commands.Author,
+    ):
+        uses = uses or 1
 
         channel = self.bot.get_channel(1002262666569592903)
 
         if channel is None or not isinstance(channel, discord.TextChannel):
             return
 
-        invite = await channel.create_invite(max_uses=users)
+        invite = await channel.create_invite(max_uses=uses)
+        try:
+            await member.send(f"Here is the invite: {invite.url}")
+        except discord.Forbidden:
+            await ctx.author.send(
+                f"I was unable to DM the member, heres the invite: {invite.url}"
+            )
 
-        await ctx.author.send(f"Here is the invite: {invite.url}")
         await ctx.message.add_reaction("\U0001f44d")
