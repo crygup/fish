cfb8a871b84eac8cf89a09b17baa39e1ce7c25ac
curiositly <curiositlyxd@gmail.com>
2022-09-05 12:23:27 -0800

    Add google search command

--- src/cogs/tools/other.py
+++ src/cogs/tools/other.py
@@ -1,10 +1,10 @@
 import re
-from typing import Optional
+from typing import List, Optional
 
 import discord
 from bot import Bot, Context
 from discord.ext import commands
-from utils import TenorUrlConverter, human_join
+from utils import ImageBytesPageSource, Pager, TenorUrlConverter, human_join, to_thread
 
 from ._base import CogBase
 
@@ -137,3 +137,28 @@ class OtherCommands(CogBase):
     #    )
     #
     #    await ctx.send(embed=embed)
+
+    @commands.group(name="google", aliases=("g",))
+    async def google(self, ctx: Context, *, query: str):
+        url = f"https://customsearch.googleapis.com/customsearch/v1?cx={self.bot.config['keys']['google-id']}&q={query}&key={self.bot.config['keys']['google-search']}"
+
+        await ctx.trigger_typing()
+        async with self.bot.session.get(url) as r:
+            data = await r.json()
+
+            embed = discord.Embed(color=self.bot.embedcolor)
+            embed.set_footer(
+                text=f"About {data['searchInformation']['formattedTotalResults']} results ({data['searchInformation']['formattedSearchTime']} seconds)"
+            )
+
+            embed.title = f"Google Search - {query}"[:256]
+
+            text = ""
+            items = data["items"][:5]
+
+            for item in items:
+                text += f"[{item['title']}]({item['link']})\n{item['snippet']}\n\n"
+
+            embed.description = text
+
+        await ctx.send(embed=embed)
