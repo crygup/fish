c56222cfd0d105e3acff7e3f4a4b5991ef7d4621
curiositly <curiositlyxd@gmail.com>
2022-09-06 08:57:09 -0800

    Add dropdown to results

--- src/cogs/search/youtube.py
+++ src/cogs/search/youtube.py
@@ -1,66 +1,107 @@
+from __future__ import annotations
+import textwrap
+
+from typing import Any, Dict, List, Literal, Tuple
+from typing import Literal as L
+from typing import Union
+
 import discord
 from bot import Context
 from discord.ext import commands
-from utils import response_checker
+from utils import response_checker, AuthorView
+
 from ._base import CogBase
+
+id_converter = {
+    "video": "videoId",
+    "channel": "channelId",
+    "playlist": "playlistId",
+}
+
+link_converter = {
+    "video": "watch?v=",
+    "channel": "channel/",
+    "playlist": "playlist?list=",
+}
 
 
 class YoutubeCommands(CogBase):
-    @commands.group(name="youtube", aliases=("yt",), invoke_without_command=True)
-    async def youtube(self, ctx: Context, *, query: str):
+    async def search_method(
+        self,
+        ctx: Context,
+        query: str,
+        type: str,
+    ):
         url = f"https://www.googleapis.com/youtube/v3/search"
         params = {
             "q": query,
             "key": self.bot.config["keys"]["google-search"],
             "part": "snippet",
-            "type": "video",
-            "maxResults": 1,
+            "type": type,
+            "maxResults": 25,
         }
+
         await ctx.trigger_typing()
         async with self.bot.session.get(url, params=params) as r:
             response_checker(r)
             data = await r.json()
 
-            url = f"https://www.youtube.com/watch?v={data['items'][0]['id']['videoId']}"
+            url = f"https://www.youtube.com/{link_converter[type]}{data['items'][0]['id'][id_converter[type]]}"
 
-        await ctx.send(url)
+        videos = data["items"]
+        view = YoutubeView(ctx, videos, type)
+        await ctx.send(url, view=view)
+
+    @commands.group(name="youtube", aliases=("yt",), invoke_without_command=True)
+    async def youtube(self, ctx: Context, *, query: str):
+        await self.search_method(ctx, query, "video")
 
     @youtube.group(name="channel", aliases=("ch",))
     async def youtube_channel(self, ctx: Context, *, query: str):
-        url = f"https://www.googleapis.com/youtube/v3/search"
-        params = {
-            "q": query,
-            "key": self.bot.config["keys"]["google-search"],
-            "part": "snippet",
-            "type": "channel",
-            "maxResults": 1,
-        }
-        await ctx.trigger_typing()
-        async with self.bot.session.get(url, params=params) as r:
-            response_checker(r)
-            data = await r.json()
-
-            url = (
-                f"https://www.youtube.com/channel/{data['items'][0]['id']['channelId']}"
-            )
-
-        await ctx.send(url)
+        await self.search_method(ctx, query, "channel")
 
     @youtube.group(name="playlist", aliases=("pl",))
     async def youtube_playlist(self, ctx: Context, *, query: str):
-        url = f"https://www.googleapis.com/youtube/v3/search"
-        params = {
-            "q": query,
-            "key": self.bot.config["keys"]["google-search"],
-            "part": "snippet",
-            "type": "playlist",
-            "maxResults": 1,
-        }
-        await ctx.trigger_typing()
-        async with self.bot.session.get(url, params=params) as r:
-            response_checker(r)
-            data = await r.json()
+        await self.search_method(ctx, query, "playlist")
 
-            url = f"https://www.youtube.com/playlist?list={data['items'][0]['id']['playlistId']}"
 
-        await ctx.send(url)
+class YoutubeDropdown(discord.ui.Select):
+    def __init__(self, videos: List[Dict[Any, Any]], type: str):
+        self._type = type
+        self.videos = videos
+
+        start = 0
+        options = []
+        for vid in videos:
+            options.append(
+                discord.SelectOption(
+                    label=textwrap.shorten(vid["snippet"]["title"], width=100),
+                    value=str(start),
+                    emoji="<:yt:1016728285905965056>",
+                )
+            )
+            start += 1
+
+        super().__init__(
+            placeholder=textwrap.shorten(videos[0]["snippet"]["title"], width=100),
+            options=options,
+        )
+
+    async def callback(self, interaction: discord.Interaction) -> Any:
+        if interaction.message is None:
+            await interaction.response.defer()
+            return
+
+        index = self.values[0]
+        link = link_converter[self._type]
+        data = self.videos[int(index)]
+        url = f"https://www.youtube.com/{link}{data['id'][id_converter[self._type]]}"
+        self.placeholder = data["snippet"]["title"]
+        await interaction.message.edit(content=url, view=self.view)
+        await interaction.response.defer()
+
+
+class YoutubeView(AuthorView):
+    def __init__(self, ctx: Context, videos: List[Dict[Any, Any]], type):
+        super().__init__(ctx)
+        self.add_item(YoutubeDropdown(videos, type))
