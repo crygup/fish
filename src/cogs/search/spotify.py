0631651a8f088065726cb60f3a7b89a2e233dcd3
curiositly <curiositlyxd@gmail.com>
2022-09-06 10:35:04 -0800

    Update search and cover command

--- src/cogs/search/spotify.py
+++ src/cogs/search/spotify.py
@@ -51,7 +51,7 @@ class SpotifyCommands(CogBase):
         if key is None:
             await self.set_spotify_key()
 
-    async def get_spotify_url(self, query: str) -> str:
+    async def get_spotify_search_data(self, query: str) -> Dict:
         url = "https://api.spotify.com/v1/search"
 
         headers = {
@@ -66,15 +66,27 @@ class SpotifyCommands(CogBase):
             if results["tracks"]["items"] == []:
                 raise ValueError("Couldn't find any tracks.")
 
-        return results["tracks"]["items"][0]["external_urls"]["spotify"]
+        return results["tracks"]["items"][0]
 
     @commands.command(name="spotify", aliases=("s",))
     @commands.before_invoke(set_key)
-    async def spotify(self, ctx: Context, *, query: str):
+    async def spotify(self, ctx: Context, *, query: Optional[str]):
         await ctx.trigger_typing()
+        if not query:
+            name = await get_lastfm(ctx.bot, ctx.author.id)
+            info = await LastfmClient(
+                self.bot, "2.0", "user.getrecenttracks", "user", name
+            )
 
-        url = await self.get_spotify_url(query)
-        await ctx.send(url)
+            if info["recenttracks"]["track"] == []:
+                raise ValueError("No recent tracks found for this user.")
+            track = info["recenttracks"]["track"][0]
+            to_search = f"{track['name']} artist:{track['artist']['#text']}"
+        else:
+            to_search = query
+
+        data = await self.get_spotify_search_data(to_search)
+        await ctx.send(data["external_urls"]["spotify"])
 
     @commands.command(name="cover", aliases=("co",))
     async def cover(self, ctx: Context, *, query: Optional[str]):
@@ -93,5 +105,8 @@ class SpotifyCommands(CogBase):
         else:
             to_search = query
 
-        url = await self.get_spotify_url(to_search)
-        await ctx.send(url)
+        data = await self.get_spotify_search_data(to_search)
+        url = data["album"]["images"][0]["url"]
+        fp = await ctx.to_bytesio(url)
+        file = discord.File(fp=fp, filename="cover.png")
+        await ctx.send(file=file)
