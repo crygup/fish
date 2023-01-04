3acb9707bc0e6ae9004c86c30ff37d1ee27264b7
curiositly <curiositlyxd@gmail.com>
2023-01-03 22:41:00 -0900

    remove old wordle command

--- src/cogs/tools/other.py
+++ src/cogs/tools/other.py
@@ -101,68 +101,6 @@ class OtherCommands(CogBase):
 
         await ctx.send(f"Here is the real URL: {url}")
 
-    # @commands.command(name="wordle", aliases=("word",), hidden=True)
-    # @commands.is_owner()
-    # async def wordle(self, ctx: Context, *, flags: str):
-    #    """This uses flags to solve a wordle problem.
-    #
-    #    Use underscores to spcify blanks.
-    #    Example: h_r___
-    #
-    #    Flags:
-    #        -jeyy    - Uses jeyybot wordle list (this is default true for the jeyybot wordle command)
-    #        -correct - letters that are correct
-    #        -invalid - letters that are not used
-    #    """
-    #    words = self.bot.words
-    #
-    #    parser = Arguments(add_help=False, allow_abbrev=False)
-    #    parser.add_argument("-j", "-jeyy", action="store_true", default=True)
-    #    parser.add_argument("-c", "-correct", type=str)
-    #    parser.add_argument("-i", "-invalid", type=str)
-    #
-    #    args = parser.parse_args(shlex.split(flags))
-    #
-    #    if args.j:
-    #        words = self.bot.jeyy_words
-    #
-    #    word = ["_", "_", "_", "_", "_"]
-    #    letters_to_skip = [letter for letter in args.i] if args.i else []
-    #
-    #    letters_to_use = "abcdefghijklmnopqrstuvwxyz"
-    #
-    #    if letters_to_skip:
-    #        letters_to_use = re.sub(f"[{''.join(letters_to_skip)}]", "", letters_to_use)
-    #
-    #    if args.c:
-    #        if len(args.c) != 5:
-    #            await ctx.send("The word must be 5 letters long.")
-    #            return
-    #
-    #        word = [letter for letter in args.c]
-    #        for i, letter in enumerate(word):
-    #            if letter == "_":
-    #                word[i] = f"[{letters_to_use}]{{1}}"
-    #            else:
-    #                word[i] = f"[{letter}]{{1}}"
-    #
-    #    pattern = re.compile("".join(word))
-    #
-    #    guessed_words = [word for word in words if pattern.match(word)]
-    #
-    #    if guessed_words == []:
-    #        await ctx.send("No words found.")
-    #        return
-    #
-    #    formatted = guessed_words[:10]
-    #    embed = discord.Embed(color=self.bot.embedcolor)
-    #    embed.title = f"Possible words found ({len(formatted)}/{len(guessed_words):,})"
-    #    embed.description = human_join(
-    #        [f"**`{word}`**" for word in formatted], final="and"
-    #    )
-    #
-    #    await ctx.send(embed=embed)
-
     @commands.command(name="urban")
     async def urban(self, ctx: Context, *, word: str):
         """Search for a word on urban
